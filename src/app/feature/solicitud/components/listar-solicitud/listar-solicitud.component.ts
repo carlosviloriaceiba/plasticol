54e48af4f725c0473bb425c3a3a92a85576d6377
Carlos Viloria <carlos.viloria@ceiba.com.co>
2021-08-03 13:00:05 -0500

    Regla de negocio cancelar solicitud

--- src/app/feature/solicitud/components/listar-solicitud/listar-solicitud.component.ts
+++ src/app/feature/solicitud/components/listar-solicitud/listar-solicitud.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { faEye, faPencilAlt,  faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBan, faEye,  faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { Solicitud } from '@solicitud/shared/model/solicitud';
 import { SolicitudService } from '@solicitud/shared/service/solicitud.service';
 import swal from 'sweetalert2';
 import * as moment from 'moment';
 import { Observable } from 'rxjs';
+import { ConfigModal } from '@shared/model/config-modal';
 
 @Component({
   selector: 'app-listar-solicitud',
@@ -13,9 +14,10 @@ import { Observable } from 'rxjs';
 })
 export class ListarSolicitudComponent implements OnInit {
   public solicitudes: Observable<Solicitud[]>;
-  public faPencilAlt = faPencilAlt;
+  public faBan = faBan;
   public faTrashAlt = faTrashAlt;
   public faEye = faEye;
+  public solicitudActual: Solicitud;
   constructor(private solicitudService: SolicitudService) { }
 
   ngOnInit(): void {
@@ -23,33 +25,64 @@ export class ListarSolicitudComponent implements OnInit {
   }
 
   checkEliminar(solicitud: Solicitud){
-
+    this.solicitudActual = solicitud;
     swal.fire({
       title: 'Estas seguro de eliminar?',
       text: `Esta acción es ireversible!`,
       icon: 'warning',
-      showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
+      showCancelButton: true,
       confirmButtonText: 'Borrar'
     }).then((result) => {
       if (result.isConfirmed) {
-       this.eliminar(solicitud);
+        const data = JSON.parse( JSON.stringify(solicitud));
+        data.deleted_at = moment().format('YYYY-MM-DD hh:mm:ss');
+        const config: ConfigModal = { titulo:  'Borrado!',
+                                      mensaje: 'Tu solicitud ha sido borrada.',
+                                      tipo: 'success'
+                                    };
+        this.actualizar(data, config);
       }
     });
   }
+  cancelar(solicitud: Solicitud){
+    this.solicitudActual = solicitud;
+    swal.fire({
+      title: 'Estas seguro de cancelar?',
+      text: `Esta acción es ireversible!`,
+      icon: 'warning',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      showCancelButton: true,
+      confirmButtonText: 'Si'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const data = JSON.parse( JSON.stringify(solicitud));
+        data.status = 'cancel';
+        const config: ConfigModal = { titulo:  'Cancelada!',
+                                      mensaje: 'Tu solicitud ha sido cancelada.',
+                                      tipo: 'success'
+                                    };
+        this.actualizar(data, config);
+      }
+    });
 
-  eliminar(solicitud: Solicitud){
-    const data = JSON.parse( JSON.stringify(solicitud));
-    data.deleted_at = moment().format('YYYY-MM-DD hh:mm:ss');
+  }
+
+  actualizar(solicitud: Solicitud, configModal: ConfigModal){
+
     this.solicitudService.actualizar(solicitud).subscribe( (borrado) => {
       if (borrado){
-        solicitud.deleted_at = data.deleted_at;
-        swal.fire(
-          'Borrado!',
-          'Tu solicitud a sido borrada.',
-          'success'
-        );
+        this.solicitudActual = solicitud;
+        if (configModal){
+          swal.fire(
+            configModal.titulo,
+            configModal.mensaje,
+            configModal.tipo
+          );
+        }
+
       }
 
     });
---
+++ src/app/shared/model/config-modal.ts
@@ -0,0 +1,7 @@
+import { SweetAlertIcon } from 'sweetalert2';
+
+export interface ConfigModal {
+    titulo: string;
+    mensaje: string;
+    tipo: SweetAlertIcon;
+}
