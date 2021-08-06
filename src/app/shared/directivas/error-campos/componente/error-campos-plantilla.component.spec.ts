512398239b266753a10ee31076d5adf1d823a5aa
Carlos Viloria <carlos.viloria@ceiba.com.co>
2021-08-06 14:28:40 -0500

    Ajustado pruebas error campos covertura

--- src/app/shared/directivas/error-campos/componente/error-campos-plantilla.component.spec.ts
+++ src/app/shared/directivas/error-campos/componente/error-campos-plantilla.component.spec.ts
@@ -22,4 +22,27 @@ describe('ErrorCamposPlantillaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deberia chequear set text', () => {
+    const text = spyOnProperty(component, 'text', 'set').and.callThrough();
+
+    component.text = 'Nuevo mensaje';
+
+    expect(text).toHaveBeenCalledWith('Nuevo mensaje');
+
+  });
+
+  it('deberia chequear set text  iguales', () => {
+    component.mensajeError = 'Nuevo mensaje';
+    const primerValor = component.mensajeError;
+
+    const segundoValor = spyOnProperty(component, 'text', 'set').and.callThrough();
+
+
+    component.text = 'Nuevo mensaje';
+
+
+    expect(segundoValor).toHaveBeenCalledWith(primerValor);
+
+  });
 });
